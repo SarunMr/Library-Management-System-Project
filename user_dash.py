e3a67059fa201f425c1213b85e2a1b60a06e4bba
Sarun Maharjan <sarunmaharjan38@gmail.com>
2024-07-18 18:28:24 +0545

    userdashboard_menubar_createed!!!

--- user_dash.py
+++ user_dash.py
@@ -1,23 +1,113 @@
 from tkinter import *
 from PIL import *
-
-
 #theme color.
 TEXTCOLOR ="#FFFFFF"
 FRAME_TITLEBAR_BG ="#008A44"
+FRAME_MENUBAR_BG = "#262626"
+SWITCH_COLOR = "#5F5F5F"
 
 window = Tk()
-window.grid_columnconfigure(1, weight=1)
+
+#icons
+menubutton_icon = PhotoImage(file="icons_lib/menu_button.png")
+menubuttontoggle_icon = PhotoImage(file="icons_lib/cross.png")
+dashboard_icon = PhotoImage(file="icons_lib/dash_icon.png")
+request_books_icon = PhotoImage(file="icons_lib/request_books_icon.png")
+return_books_icon = PhotoImage(file="icons_lib/return_ic.png")
+books_log_icon =PhotoImage(file="icons_lib/log_icon.png")
+profile_icon =PhotoImage(file="icons_lib/profile.png")
+logout_icon = PhotoImage(file="icons_lib/logout_icon.png")
+
+
+window.grid_columnconfigure(0, weight=0)
+window.grid_columnconfigure(1,weight=1)
+window.grid_rowconfigure(1,weight=1)
+
+def update_label_color_on_tap(n):
+    label_dict = {"1":lib_dash_label,"2":lib_requestbook_label,"3":lib_returnbook_label,"4":lib_log_label,"5":lib_profile_label}
+    btn_dict = {"1":lib_dash_button,"2":lib_request_book_button,"3":lib_return_book_button,"4":lib_log_button,"5":lib_profile_button}
+    for label in label_dict.keys():
+        if label == n:
+            label_dict[label].config(bg=SWITCH_COLOR)
+            btn_dict[label].config(bg=SWITCH_COLOR)
+        else:
+            label_dict[label].config(bg=FRAME_MENUBAR_BG)
+            btn_dict[label].config(bg=FRAME_MENUBAR_BG)
+
+def default_menubar():
+    labellst = [lib_dash_label,lib_requestbook_label,lib_returnbook_label,lib_log_label,lib_profile_label]
+    for label in labellst:
+        label.config(width=10)
+    
+    lib_menu_button.config(image=menubutton_icon)
+    lib_dash_button.config(text="",width=45,)
+    lib_request_book_button.config(text="",width=45)
+    lib_return_book_button.config(text="",width=45)
+    lib_log_button.config(text="",width=45)
+    lib_profile_button.config(text="",width=45)
+    lib_logout_button.config(text="")
+    lib_menu_button.config(command=extend_menubar)
+
+
+def extend_menubar():
+    labellst = [lib_dash_label,lib_requestbook_label,lib_returnbook_label,lib_log_label,lib_profile_label]
+    for label in labellst:
+        label.config(width=40)
+
+    lib_menu_button.config(image=menubuttontoggle_icon,padx=13,pady=30)
+    lib_dash_button.config(text="Dashboard",width=200,padx=20,anchor="w")
+    lib_request_book_button.config(text="Request Books",width=200,padx=20,anchor="w")
+    lib_return_book_button.config(text="Return Books",width=200,padx=20,anchor="w")
+    lib_log_button.config(text="Books Log",width=200,padx=20,anchor="w")
+    lib_profile_button.config(text="Your Profile",width=200,padx=20,anchor="w")
+    lib_logout_button.config(text="Log out")
+    lib_menu_button.config(command=default_menubar)
+
+
+
 frame_titlebar =Frame(window,bg=FRAME_TITLEBAR_BG)
 frame_titlebar.columnconfigure(0,weight=0)
-frame_titlebar.columnconfigure(1,weight=2)
-lib_menu_button=Button(frame_titlebar,text="not set")
+frame_titlebar.columnconfigure(1,weight=1)
+lib_menu_button=Button(frame_titlebar,image=menubutton_icon,bg=FRAME_TITLEBAR_BG,borderwidth=0,activebackground=FRAME_TITLEBAR_BG,command=extend_menubar)
 lib_name = Label(frame_titlebar,text="Lorem Library",font=("Arial",35,"bold"),fg=TEXTCOLOR,bg=FRAME_TITLEBAR_BG,anchor="w")
+#placingwidgets
+lib_menu_button.grid(row= 0,column=0,padx=10,pady=10)
+lib_name.grid(row=0,column=1,padx=(35,0),sticky="w")
+
+
+frame_menubar=Frame(window,bg=FRAME_MENUBAR_BG)
+lib_dash_label = Label(frame_menubar,bg=SWITCH_COLOR,height=4,width=10)
+lib_dash_button =Button(frame_menubar,image=dashboard_icon,text="",font=("Arial",15,"bold"),compound=LEFT,fg=TEXTCOLOR,bg=SWITCH_COLOR,borderwidth=0,activebackground=SWITCH_COLOR,command=lambda:update_label_color_on_tap("1"))
+lib_requestbook_label = Label(frame_menubar,bg=FRAME_MENUBAR_BG,height=4,width=10)
+lib_request_book_button =Button(frame_menubar,image=request_books_icon,font=("Arial",15,"bold"),compound=LEFT,fg=TEXTCOLOR,bg=FRAME_MENUBAR_BG,borderwidth=0,activebackground=SWITCH_COLOR,command=lambda:update_label_color_on_tap("2"))
+lib_returnbook_label = Label(frame_menubar,bg=FRAME_MENUBAR_BG,height=4,width=10)
+lib_return_book_button =Button(frame_menubar,image=return_books_icon,font=("Arial",15,"bold"),compound=LEFT,fg=TEXTCOLOR,bg=FRAME_MENUBAR_BG,borderwidth=0,activebackground=SWITCH_COLOR,command=lambda:update_label_color_on_tap("3"))
+lib_log_label = Label(frame_menubar,bg=FRAME_MENUBAR_BG,height=4,width=10)
+lib_log_button =Button(frame_menubar,image=books_log_icon,font=("Arial",15,"bold"),compound=LEFT,fg=TEXTCOLOR,bg=FRAME_MENUBAR_BG,borderwidth=0,activebackground=SWITCH_COLOR,command=lambda:update_label_color_on_tap("4"))
+lib_profile_label = Label(frame_menubar,bg=FRAME_MENUBAR_BG,height=4,width=10)
+lib_profile_button =Button(frame_menubar,image=profile_icon,font=("Arial",15,"bold"),compound=LEFT,fg=TEXTCOLOR,bg=FRAME_MENUBAR_BG,borderwidth=0,activebackground=SWITCH_COLOR,command=lambda:update_label_color_on_tap("5"))
+lib_logout_button =Button(frame_menubar,image=logout_icon,bg=FRAME_MENUBAR_BG,font=("Arial",15,"bold"),compound=LEFT,fg="#FF4500",borderwidth=0,activebackground=FRAME_MENUBAR_BG,)
+
+#placing inside menuframe
+lib_dash_label.grid(row=0,column=0,padx=13,pady=(30,35))
+lib_dash_button.grid(row=0,column=0,padx=13,pady=(30,35))
+lib_requestbook_label.grid(row=1,column=0,padx=13,pady=(0,35))
+lib_request_book_button.grid(row=1,column=0,padx=13,pady=(0,35))
+lib_returnbook_label.grid(row=2,column=0,padx=13,pady=(0,35))
+lib_return_book_button.grid(row=2,column=0,padx=13,pady=(0,35))
+lib_log_label.grid(row=3,column=0,padx=13,pady=(0,35))
+lib_log_button.grid(row=3,column=0,padx=13,pady=(0,35))
+lib_profile_label.grid(row=4,column=0,padx=13,pady=(0,35))
+lib_profile_button.grid(row=4,column=0,padx=13,pady=(0,35))
+lib_logout_button.grid(row=5,column=0,padx=13,pady=(30))
+
+
+trylabel = Label(window,text="check",fg="white",bg="black")
 
 
 
-lib_menu_button.grid(row= 0,column=0,padx=20,pady=(10,0))
-lib_name.grid(row=0,column=1,padx=(25,0),sticky="we")
-
-frame_titlebar.grid(row=0, column=1, sticky="ew")
+#placing frames
+frame_titlebar.grid(row=0, column=0,columnspan=2,sticky="we")
+frame_menubar.grid(row=1,column=0,sticky="wsn")
+trylabel.grid(row=1,column=1,sticky="news")
 window.mainloop()
